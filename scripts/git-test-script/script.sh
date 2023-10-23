614462c4086ec60d7a1ecd7e8da2fa118d001543
Josh Bourdon <bourdojo@gvsu.edu>
2023-10-23 11:47:33 -0400

    Update script.sh

--- scripts/git-test-script/script.sh
+++ scripts/git-test-script/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This is just a test script for git4jss
 
-echo "Test after override URL. SSL enabled. Secret used."
+echo "Test after override URL. SSL enabled. Secret used. fixed url"
