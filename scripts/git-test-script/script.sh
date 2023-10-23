906ccbb14c40dbcef9c5f3cfea36ccc227838bfc
Josh Bourdon <bourdojo@gvsu.edu>
2023-10-23 15:16:19 -0400

    Update script.sh

--- scripts/git-test-script/script.sh
+++ scripts/git-test-script/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This is just a test script for git4jss
 
-echo "Test after override URL. SSL enabled. Secret used. github alt. github-webhok URL. took out proxy"
+echo "Test after override URL. SSL enabled. Secret used. github alt. github-webhok URL. 302"
